31960fcfabd6b6e248a8822ddde6a6d1d852180e
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-10-12 17:08:03 +0200

    try

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
 
   resources :users, concerns: :accountable, except: :destroy, :path => "members"
 
-  post 'users/search'
+  get 'users/search'
 
   resources :transfers, only: [:create] do
     member do
