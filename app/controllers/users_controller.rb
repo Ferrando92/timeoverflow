854e4b40f25d0a974912f7f98c8f3fee95106010
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-10-19 17:44:54 +0200

    refactoring a bit, creatinga new object with all the members instead of current_organization.members

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
                      ob[mem.user_id] = mem
                    end
     @organization_members = current_organization.members
-    @organization_members = @organization_members.sort_by {|member| member.user.username}
+    @organization_members.user.sort_by username
   end
 
   def show
